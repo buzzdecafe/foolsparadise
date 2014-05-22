92aa6b1441a2085f800e789cf6ad9c96c3b3b23b
Michael Hurley <m_hur@yahoo.com>
2014-05-22 11:48:51 -0400

    renaming for clarity

--- src/IO.js
+++ src/IO.js
@@ -1,7 +1,7 @@
 var compose = require("ramda").compose;
 
 function IO(run) {
-  this.run = run;
+  this.fn = fn;
 }
 
 IO.of = function(value) {
@@ -12,11 +12,11 @@ IO.of = function(value) {
 
 IO.prototype.map = function(f) {
   var io = this;
-  return new IO(compose(f, io.run));
+  return new IO(compose(f, io.fn));
 };
 
 IO.run = function(io) {
-  return io.run.apply(this, [].slice.call(arguments, 1));
+  return io.fn.apply(this, [].slice.call(arguments, 1));
 };
 
 IO.prototype.of = IO.of;
