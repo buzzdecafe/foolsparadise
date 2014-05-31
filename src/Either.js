ac9bd1b42c221f289f30b87de2b22ecf150796df
buzzdecafe <m_hur@yahoo.com>
2014-05-31 07:35:26 -0400

    Either monad, i think. gotta get some tests going

--- src/Either.js
+++ src/Either.js
@@ -16,6 +16,16 @@ Either.prototype.map = function(f) {
   return (isNil(this.right)) ? this : new Either(this.left, f(this.right));
 };
 
+Either.prototype.ap = function(app) {
+  return new Either(app.left, app.map(this.value));
+};
+
+// `f` must return a new Either; not sure if this impl is sufficient
+Either.prototype.chain = function(f) {
+  return f(this.right);
+};
+
+
 Either.prototype.of = Either.of;
 
 module.exports = Either;
