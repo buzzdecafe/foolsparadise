0a1b43e2f97ef6ebf33d890ffa2e141fb7358e12
buzzdecafe <m_hur@yahoo.com>
2014-05-31 10:46:56 -0400

    testing helpers, taken from fantasyland spec

---
+++ test/types.js
@@ -0,0 +1,57 @@
+
+function I(x) { return x; };
+
+function equals(a, b) {
+  return a.value === b.value;
+}
+
+function getConstructor(obj) {
+  return Object.getPrototypeOf(obj).constructor;
+}
+
+module.exports = {
+
+  isFunctor: function(obj) {
+    return function(f, g) {
+      return typeof obj.map === 'function' &&
+             equals(obj, obj.map(I)) && // identity
+             equals(obj.map(function(x) { return f(g(x)); }), obj.map(f).map(g)); // composition
+             // assert same types? JS won't help here...
+             // 
+    };
+  },
+
+  isApply: function(obj) {
+    return function(obj2) {
+      return isFunctor(obj) &&
+             typeof obj.ap === 'function' &&
+             obj.ap(obj2) === obj.value(obj2.value);
+    };
+  },
+
+  isApplicative: function(obj) {
+    var C = Object.getPrototypeOf(obj).constructor;
+    return function(value) {
+      return isApply(obj) &&
+             typeof obj.of === 'function' &&
+             equals(obj.of(value), new C(value));
+    };
+  },
+
+  isChain: function(obj) {
+    return function(f, g) {
+      return isApply(obj) &&
+             typeof obj.chain === 'function' &&
+             equals(obj.chain(f).chain(g), obj.chain(function(x) { return f(x).chain(g); }));
+    };
+  }
+
+  isMonad: function(obj) {
+    return isApplicative(obj) && isChain(obj);
+  }
+};
+
+
+
+
+
