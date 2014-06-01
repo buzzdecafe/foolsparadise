406e078c0d6064328fc37437737ccfdd833c8e3e
buzzdecafe <m_hur@yahoo.com>
2014-06-01 13:55:55 -0400

    Apply test for Maybe

--- test/maybe.test.js
+++ test/maybe.test.js
@@ -7,8 +7,15 @@ var Maybe = require('../src/Maybe');
 
 describe('Maybe', function() {
   var m = Maybe(1);
-  it('is a functor', function() {
+
+  it('is a Functor', function() {
     assert.equal(true, types.isFunctor(m, R.multiply(2), R.add(1)));
+  });
+
+  it('is an Apply', function() {
+    var app1 = Maybe(function(x) { return x * 10; });
+    var app2 = Maybe(10);
+    assert.equal(true, types.isApply(app1, app2));
   });
 
 });
--- test/types.js
+++ test/types.js
@@ -10,42 +10,42 @@ function getConstructor(obj) {
   return Object.getPrototypeOf(obj).constructor;
 }
 
+function isFunctor(obj, f, g) {
+  return typeof obj.map === 'function' &&
+         equals(obj, obj.map(I)) && // identity
+         equals(obj.map(function(x) { return f(g(x)); }), obj.map(g).map(f)); // composition
+}
+
+function isApply(obj, obj2) {
+  var C = getConstructor(obj);
+  /*isFunctor(obj) &&*/
+  return typeof obj.ap === 'function' &&
+         equals(obj.ap(obj2), C(obj.value(obj2.value)));
+}
+
+function isApplicative(obj, value) {
+  var C = Object.getPrototypeOf(obj).constructor;
+  return isApply(obj) &&
+         typeof obj.of === 'function' &&
+         equals(obj.of(value), new C(value));
+}
+
+function isChain(obj, f, g) {
+  return isApply(obj) &&
+         typeof obj.chain === 'function' &&
+         equals(obj.chain(f).chain(g), obj.chain(function(x) { return f(x).chain(g); }));
+}
+
+function isMonad(obj) {
+  return isApplicative(obj) && isChain(obj);
+}
+
+
 module.exports = {
-
-  isFunctor: function(obj, f, g) {
-      return typeof obj.map === 'function' &&
-             equals(obj, obj.map(I)) && // identity
-             equals(obj.map(function(x) { return f(g(x)); }), obj.map(g).map(f)); // composition
-             // assert same types? JS won't help here...
-             // 
-  },
-
-  isApply: function(obj, obj2) {
-      return isFunctor(obj) &&
-             typeof obj.ap === 'function' &&
-             obj.ap(obj2) === obj.value(obj2.value);
-  },
-
-  isApplicative: function(obj, value) {
-    var C = Object.getPrototypeOf(obj).constructor;
-      return isApply(obj) &&
-             typeof obj.of === 'function' &&
-             equals(obj.of(value), new C(value));
-  },
-
-  isChain: function(obj, f, g) {
-      return isApply(obj) &&
-             typeof obj.chain === 'function' &&
-             equals(obj.chain(f).chain(g), obj.chain(function(x) { return f(x).chain(g); }));
-  },
-
-  isMonad: function(obj) {
-    return isApplicative(obj) && isChain(obj);
-  }
-
+  isFunctor: isFunctor,
+  isApply: isApply,
+  isApplicatove: isApplicative,
+  isChain: isChain,
+  isMonad: isMonad
 };
-
-
-
-
 
